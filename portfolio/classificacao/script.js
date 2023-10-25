2c428dab718eaefc02e6d09c86fb68640b328db1
Duarte, O Próprio <121031358+dduarte106@users.noreply.github.com>
2023-10-25 12:02:23 -0300

    Add files via upload

--- portfolio/classificacao/script.js
+++ portfolio/classificacao/script.js
@@ -1,15 +1,35 @@
 const circles = document.querySelectorAll('.circle');
 const submitButton = document.getElementById('submitButton');
+const myForm = document.getElementById('myForm');
+
+submitButton.addEventListener('mouseenter', () => {
+  const isAnyCircleSelected = Array.from(circles).some(c => c.classList.contains('selected'));
+  if (isAnyCircleSelected) {
+    submitButton.style.backgroundColor = 'white';
+    submitButton.style.color = 'hsl(25, 97%, 53%)';
+    submitButton.style.border = '1px solid rgb(0, 0, 0)';
+  }
+});
+
+submitButton.addEventListener('mouseleave', () => {
+  submitButton.style.backgroundColor = ''; // Reverte para a cor padrão
+  submitButton.style.color = ''; // Reverte para a cor padrão
+  submitButton.style.border = 'none'; // Remove a borda
+});
 
 circles.forEach(circle => {
   circle.addEventListener('click', () => {
-    // Remove a classe 'selected' de todos os círculos
     circles.forEach(c => c.classList.remove('selected'));
-
-    // Adicione a classe 'selected' apenas ao círculo clicado
     circle.classList.add('selected');
-
-    // Ative o botão de enviar
-    submitButton.disabled = false;
+    const isAnyCircleSelected = Array.from(circles).some(c => c.classList.contains('selected'));
+    submitButton.disabled = !isAnyCircleSelected;
   });
 });
+
+myForm.addEventListener('submit', (event) => {
+  const isAnyCircleSelected = Array.from(circles).some(c => c.classList.contains('selected'));
+  if (!isAnyCircleSelected) {
+    event.preventDefault();
+  }
+});
+
