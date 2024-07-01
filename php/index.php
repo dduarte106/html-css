85aac641078fc582f63b18bdf5f1ae85a636808f
Duarte, O Próprio <121031358+dduarte106@users.noreply.github.com>
2024-06-30 22:40:57 -0300

    A

--- php/index.php
+++ php/index.php
@@ -7,7 +7,9 @@
 </head>
 <body>
     <h1>
-
+        <?php
+            echo "Olá, mundo \u{1F30E}";
+        ?>
     </h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste incidunt excepturi, nemo, commodi eaque adipisci laborum quam aspernatur aperiam suscipit ea ex obcaecati corrupti quaerat perferendis quasi odit nam quo.</p>
 </body>
